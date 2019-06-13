129df14a05d80b9a34995dbc3089df0fc3c61b3b
SergeiN <Snaumenkov@bk.ru>
2019-06-13 03:00:26 +0300

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String[] args)
     {
 	 System.out.println("HELLO WORLD");
-	 System.out.println("Stypid GIT");
+	 
 	 for( int i = 0; i < 13 ; i++)
 	     System.out.println(" XA ");
     }
